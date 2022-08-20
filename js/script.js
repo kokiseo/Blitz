e8d9062291aa53bfd885133d92b719be89be0b1d
Koki SEO <kokihfc21@gmail.com>
2022-08-20 20:14:16 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  var topBtn = $('.pagetop');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  //ドロワーメニュー
+  $("#MenuButton").click(function () {
+    // $(".l-drawer-menu").toggleClass("is-show");
+    // $(".p-drawer-menu").toggleClass("is-show");
+    $(".js-drawer-open").toggleClass("open");
+    $(".drawer-menu").toggleClass("open");
+    $("html").toggleClass("is-fixed");
+
+  });
+
+
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+
+  $(document).on('click', 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $('header').innerHeight();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    return false;
+  });
+
+  // モーダルウィンドウを開く
+  $('.js-modal-open').on('click', function(){
+    var target = $(this).data('target');
+    var modal = document.getElementById(target);
+    scrollPosition = $(window).scrollTop();
+
+    $('body').addClass('fixed').css({'top': -scrollPosition});
+    $(modal).fadeIn();
+    return false;
+  });
+
+  // モーダルウィンドウを閉じる
+  $('.js-modal-close').on('click', function(){
+    $('body').removeClass('fixed');
+    window.scrollTo( 0 , scrollPosition );
+    $('.js-modal').fadeOut();
+    return false;
+  });
+
+});
